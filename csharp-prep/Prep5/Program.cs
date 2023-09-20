2efebd79b72ad95620baddf4919285a1ed20ed0b
bryantsmith125 <smi17048@byui.edu>
2023-09-20 10:29:53 -0600

    Adding comments to my code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -13,12 +13,12 @@ class Program
 
         DisplayResult(username, squarednumber);
     }
-
+    // Function for displaying the welcome message
     static void DisplayWelcomeMessage()
     {
         Console.WriteLine("Welcome to the program!");
     }
-
+    // Function to ask for username
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
@@ -26,7 +26,7 @@ class Program
 
         return name;
     }
-
+    // Function to ask for a favorite number
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
@@ -34,13 +34,13 @@ class Program
 
         return number;
     }
-
+    // Function to square the favorite number
     static int SquareNumber(int number)
     {
         int square = number * number;
         return square;
     }
-
+    // Function to show the result of the number
     static void DisplayResult(string name, int square)
     {
         Console.WriteLine($"{name}, the square of your number is {square}");
