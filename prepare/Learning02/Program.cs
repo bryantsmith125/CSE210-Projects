527682485af379f4dcb560602b444c8b4b222133
bryantsmith125 <smi17048@byui.edu>
2023-09-27 13:32:15 -0600

    Learning 02 Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,8 +9,8 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        job1._startYear = 2017;
-        job1._endYear = 2023;
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
         Job job2 = new Job();
         job2._jobTitle = "Manager";
