a5cd6ab260534a338e22226248ecb5a5652cfcab
bryantsmith125 <smi17048@byui.edu>
2023-11-29 12:01:06 -0700

    First steps on Foundation 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+
+        Console.Write("Foundation Program 1 - Abstraction with YouTube Videos\n");
+        Console.Write("------------------------------------------------------\n");
+        Console.WriteLine("(Abstraction: The process of turning complex ideas into simple ones.)\n");
+
+
+        Comment comment1 = new Comment();
+        comment1._name = "Nick Fury";
+        comment1._commentText = "Nice to see you are blowing off my calls for this...";
+        Comment comment2 = new Comment();
+        comment2._name = "Thor of Asguard";
+        comment2._commentText = "If you want to see real talent, check out my channel!";
+        Comment comment3 = new Comment();
+        comment3._name = "Steve Rogers";
+        comment3._commentText = "Language!";
+        Comment comment4 = new Comment();
+        comment4._name = "Pepper Potts";
+        comment4._commentText = "I thought you were at a business meeting!";
+        Comment comment5 = new Comment();
+        comment5._name = "Shuri";
+        comment5._commentText = "I need to make some modifications to your suit.";
+        Comment comment6 = new Comment();
+        comment6._name = "Eric Killmonger";
+        comment6._commentText = "I'm coming back to take what's mine cuz!";
+        Comment comment7 = new Comment();
+        comment7._name = "M'Baku";
+        comment7._commentText = "Why do we leave the fate of Wakanda to T'Challa? He will lead us to ruin!";
+        Comment comment8 = new Comment();
+        comment8._name = "Black Widow";
+        comment8._commentText = "Hey big guy. The sun is getting real low.";
+        Comment comment9 = new Comment();
+        comment9._name = "Tony Stark";
+        comment9._commentText = "I must say, I am a huge fan of your work! Especially when you turn into a big green rage monster!";
+        Comment comment10 = new Comment();
+        comment10._name = "Loki";
+        comment10._commentText = "Enough! I am a god. I will not be bullied by the likes of you!";
+        Comment comment11 = new Comment();
+        comment11._name = "Abomination";
+        comment11._commentText = "I could do it better.";
+        Comment comment12 = new Comment();
+        comment12._name = "Thor of Asguard";
+        comment12._commentText = "I am still the strongest Avenger!";
+
+
     }
 }
