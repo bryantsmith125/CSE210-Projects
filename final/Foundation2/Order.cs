f3a337707a92b2d11e07f0a9b6d314bb1ed66ec8
bryantsmith125 <smi17048@byui.edu>
2023-12-02 13:46:15 -0700

    Finish Foundation 2

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -6,9 +6,10 @@ class Order
     private List<Product> _products;
     private Customer _customer;
     
-    public Order(Customer customer)
+    public Order(Customer customer, List<Product> products)
     {
         _customer = customer;
+        _products = products;
     }
 
     public string GetLabel()
@@ -17,4 +18,50 @@ class Order
         string customerAddress = _customer.GetAddress();
         return $"{customerName}\n{customerAddress}";
     }
+    public decimal CalculateTotal()
+    {
+        decimal totalCost = 0;
+        foreach (Product product in _products)
+        {
+            totalCost += product.CalculateTotalPrice();
+        }
+
+        decimal shippingCost;
+        if (_customer.findUSA())
+        {
+            shippingCost = 5;
+        }
+            else
+        {
+            shippingCost = 35;
+        }
+        
+        return totalCost + shippingCost;
+    }
+
+    public string GetPackLabel()
+    {
+        string packingLabel = "";
+        foreach (Product product in _products)
+        {
+            packingLabel += $"Product Name: {product.GetName()} ({product.GetProductId()})\n";
+        }
+
+        return packingLabel;
+    }
+
+        public decimal ShippingTotal()
+    {
+        decimal shippingCost;
+        if (_customer.findUSA())
+        {
+            shippingCost = 5;
+        }
+            else
+        {
+            shippingCost = 35;
+        }
+        
+        return shippingCost;
+    }  
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -8,9 +8,22 @@ class Program
         Console.Write("---------------------------------------------------------\n");
         Console.WriteLine("(Encapsulation: The act of enclosing something, as if it were in a capsule.)\n");
 
-        Address address1 = new Address("879 Everett Pl Apt. 1, "Rexburg", "ID", "USA");
+        Address address1 = new Address("879 Everett Pl Apt. 1", "Rexburg", "ID", "USA");
+        Address address2 = new Address("5103 Eureka Way", "Vancouver", "WA", "USA");
+        Address address3 = new Address("1110 NE 100th ave", "Vancouver", "WA", "USA");
 
         Customer customer1 = new Customer("Bryant Smith", address1);
+        Customer customer2 = new Customer("Jean Smith", address2);
+        Customer customer3 = new Customer("Janae Smith", address3);
+
+        Product product1 = new Product("Large TV", "TV-2468", 175.0m, 1);
+        Product product2 = new Product("Christmas Tree", "TRE-123", 75.0m, 1);
+        Product product3 = new Product("Griddle", "GRI-345", 65.0m, 2);
+        Product product4 = new Product("Shoes", "SHO-4321", 125.0m, 4);
+
+        Order order1 = new Order(customer1, new List<Product> {product1, product3});
+        Order order2 = new Order(customer2, new List<Product> {product1, product3, product4});
+        Order order3 = new Order(customer3, new List<Product> {product2, product3});
 
         Console.WriteLine("===============================================");
         Console.WriteLine("                  Order #1");
@@ -18,7 +31,43 @@ class Program
         Console.WriteLine("Shipping Label:");
         Console.WriteLine(order1.GetLabel());
         Console.WriteLine("");
+        Console.WriteLine("Shipping: $" + order1.ShippingTotal().ToString("F2"));        
+        Console.WriteLine("Total Cost: $" + order1.CalculateTotal().ToString("F2"));
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("Packing List:");
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine(order1.GetPackLabel());
+        Console.WriteLine("===============================================\n");
+        Console.WriteLine("");
 
+        Console.WriteLine("===============================================");
+        Console.WriteLine("                  Order #2");
+        Console.WriteLine("===============================================");
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order2.GetLabel());
+        Console.WriteLine("");
+        Console.WriteLine("Shipping: $" + order2.ShippingTotal().ToString("F2"));  
+        Console.WriteLine("Total Cost: $" + order2.CalculateTotal().ToString("F2"));
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("Packing List:");
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine(order2.GetPackLabel());
+        Console.WriteLine("===============================================\n");
+        Console.WriteLine("");
 
+        Console.WriteLine("===============================================");        
+        Console.WriteLine("                  Order #3");
+        Console.WriteLine("===============================================");
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order3.GetLabel());
+        Console.WriteLine("");
+        Console.WriteLine("Shipping: $" + order3.ShippingTotal().ToString("F2"));  
+        Console.WriteLine("Total Cost: $" + order3.CalculateTotal().ToString("F2"));
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine("Packing List:");
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine(order3.GetPackLabel());
+        Console.WriteLine("===============================================\n");
+        Console.WriteLine("");
     }
 }
